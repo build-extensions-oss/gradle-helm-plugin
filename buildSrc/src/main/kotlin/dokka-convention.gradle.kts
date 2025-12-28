9b051ec32b340622f0eedc9e8f2b765276d9d982
Igor Manushin <igor.manushin@pm.me>
2025-12-28 17:41:05 +0000

    migrate to new dokka generation

--- buildSrc/src/main/kotlin/dokka-convention.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.dokka.gradle.DokkaExtension
+
 plugins {
     id("org.jetbrains.dokka")
 }
@@ -35,6 +37,39 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
         }
         externalDocumentationLink {
             url.set(uri("https://docs.groovy-lang.org/latest/html/groovy-jdk/").toURL())
+        }
+    }
+}
+
+// strangely, "dokka {}" doesn't work here for Gradle 8. Worth migration later...
+extensions.configure<DokkaExtension>("dokka") {
+    val sourceSetName = "main"
+    dokkaSourceSets.getByName(sourceSetName) {
+        // this documentation is used in our project. Let's register everything to provide smooth links
+        val externalLinkNameToUrl = listOf(
+            "java" to "https://docs.oracle.com/javase/17/docs/api/",
+            "javadoc" to "https://docs.gradle.org/current/javadoc/",
+            "groovy" to "https://docs.groovy-lang.org/latest/html/groovy-jdk/",
+        )
+
+        externalDocumentationLinks {
+            externalLinkNameToUrl.forEach { (linkName, docUrl) ->
+                register(linkName) {
+                    url.set(uri(docUrl))
+                }
+            }
+        }
+
+        reportUndocumented.set(false)
+
+        val githubUrl = project.extra["github.url"] as String
+
+        sourceLink {
+            localDirectory.set(project.file("src/$sourceSetName/kotlin"))
+            remoteUrl.set(
+                uri("$githubUrl/blob/v${project.version}/${project.projectDir.relativeTo(rootDir)}/src/$sourceSetName/kotlin")
+            )
+            remoteLineSuffix.set("#L")
         }
     }
 }
