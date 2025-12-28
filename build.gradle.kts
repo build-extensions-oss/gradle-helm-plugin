80ff358c5b9b9568dcb11e0aea140b9e5c43e297
Igor Manushin <igor.manushin@pm.me>
2025-12-28 17:11:05 +0000

    migrate dokka notations

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,47 +51,6 @@ subprojects {
         }
     }
 
-    plugins.withId("org.jetbrains.dokka") {
-        // have an option to disable Dokka task for local builds
-        tasks.withType<Jar>().matching { it.name == "javadocJar" || it.name == "publishPluginJavaDocsJar" }
-            .all {
-                from(tasks.named("dokkaJavadoc"))
-            }
-
-        tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
-            dokkaSourceSets.all {
-                externalDocumentationLink {
-                    url.set(uri("https://docs.oracle.com/javase/8/docs/api/").toURL())
-                }
-                reportUndocumented.set(false)
-
-                val sourceSetName = this.name
-                val githubUrl = project.extra["github.url"] as String
-
-                sourceLink {
-                    localDirectory.set(project.file("src/$sourceSetName/kotlin"))
-                    remoteUrl.set(
-                        uri("$githubUrl/blob/v${project.version}/${project.projectDir.relativeTo(rootDir)}/src/$sourceSetName/kotlin").toURL()
-                    )
-                    remoteLineSuffix.set("#L")
-                }
-            }
-        }
-
-        plugins.withType<JavaGradlePluginPlugin> {
-            tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
-                dokkaSourceSets.all {
-                    externalDocumentationLink {
-                        url.set(uri("https://docs.gradle.org/current/javadoc/").toURL())
-                    }
-                    externalDocumentationLink {
-                        url.set(uri("https://docs.groovy-lang.org/latest/html/groovy-jdk/").toURL())
-                    }
-                }
-            }
-        }
-    }
-
     plugins.withId("com.gradle.plugin-publish") {
         val githubUrl = project.extra["github.url"] as String
 
