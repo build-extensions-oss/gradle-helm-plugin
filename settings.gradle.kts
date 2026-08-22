836286cffd14eda8094154a035ef240e2d5e0f69
Igor Manushin <igor.manushin@pm.me>
2026-08-22 15:06:56 +0100

    stop using maven central for plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,10 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
+// don't uncomment the statements below and especially - the line 'mavenCentral' due to https://github.com/renovatebot/renovate/discussions/43146#discussioncomment-16917760
+//pluginManagement {
+//    repositories {
+//        gradlePluginPortal()
+//        mavenCentral() <<<<<<<<<<<< don't add this
+//    }
+//}
 
 rootProject.name = "gradle-helm-plugin-parent"
 
